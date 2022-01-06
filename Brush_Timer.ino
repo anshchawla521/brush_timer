3a3fbb14c1ffad16c14ae200f25b31be44298ff9
Ansh <anshchawla521@gmail.com>
2022-01-06 18:21:37 +0530

    1st working code in embedded C

--- Brush_Timer.ino
+++ Brush_Timer.ino
@@ -1,4 +1,4 @@
-#define tim 20 // in seconds
+#define tim 120 // in seconds
 #define led_pin 13
 #define button_pin 2 // only 2 or 3
 #define minpulsetime 30
@@ -21,38 +21,36 @@ void isr() {
 
 void setup() {
   pinMode(led_pin , OUTPUT);
-  pinMode(button_pin,INPUT_PULLUP);
+  pinMode(button_pin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(button_pin), isr, CHANGE);
   Serial.begin(9600);
 }
 
 void loop() {
   if (is_running) {
-    if (millis() - start_time > tim*100) { // overflow automatically taken care of
-      // time up 
+    if (millis() - start_time > ((unsigned long)tim * 1000)) { // overflow automatically taken care of
+      // time up
       is_running = false;
-    }else{
-      // running and time not over 
-      time_left = int(float(minpulsetime) +float(maxpulsetime-minpulsetime)*(float(start_time + tim*100 - millis()))/float(tim*100) ) ;
-      //Serial.print("Time left ");
-      //Serial.println(time_left);
-      if(millis() -  last_blink >time_left){
+    } else {
+      // running and time not over
+      time_left = int(float(minpulsetime) + float(maxpulsetime - minpulsetime) *  (float((start_time + ((unsigned long)tim * 1000) - millis()) / 1000.0) / float(tim))   ) ; // find max limit of the operations
+      if (millis() -  last_blink > time_left) {
         // time to change state
         last_blink = millis();
         //Serial.println("toggled led");
         digitalWrite(led_pin, !digitalRead(led_pin));//toggle
       }
-      
 
-      
+
+
     }
-    
-  }else{
+
+  } else {
     digitalWrite(led_pin , LOW);
     //goto sleep
     //Serial.println("i am sleeping");
     //delay(1000);
-    
-    }
+
+  }
 
 }
